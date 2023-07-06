1eccc79a2cb54bee682e3cd9a2877c017a61d30a
Dioney Junior <dioneyfbjunior@gmail.com>
2023-07-06 12:30:56 -0300

    Implement initial logic for gerarDog() function

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,10 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  imagensCachorro = ['assets/dog.jpg', 'assets/dog2.jpg'];
+  imagemCachorro = this.imagensCachorro[0];
 
-  constructor() {}
-
+  gerarDog() {
+    console.log("novo doguinho gerado")
+    const indiceAtual = this.imagensCachorro.indexOf(this.imagemCachorro);
+    const proximoIndice = (indiceAtual + 1) % this.imagensCachorro.length;
+    this.imagemCachorro = this.imagensCachorro[proximoIndice];
+  }
 }
