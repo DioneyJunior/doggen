9933243d3b3ab0831ad220707245c0cb4805eaa4
Dioney Junior <dioneyfbjunior@gmail.com>
2023-07-06 13:31:28 -0300

    generate dogs by breed logic

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  selectedBreed: string = '';
+  selectedBreedImage: string = '';
+  breeds: string[] = [];
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+    this.fetchBreeds();
+  }
 
+  fetchBreeds() {
+    const apiUrl = 'https://dog.ceo/api/breeds/list/all';
+    this.http.get(apiUrl).subscribe(
+      (response: any) => {
+        this.breeds = Object.keys(response.message);
+        console.log('Raças disponíveis:', this.breeds);
+        if (this.breeds.length > 0) {
+          this.selectedBreed = this.breeds[0];
+          this.fetchBreedImage();
+        }
+      },
+      (error) => {
+        console.log('Erro ao obter as raças:', error);
+      }
+    );
+  }
+
+  fetchBreedImage() {
+    const apiUrl = `https://dog.ceo/api/breed/${this.selectedBreed}/images/random`;
+    this.http.get(apiUrl).subscribe(
+      (response: any) => {
+        this.selectedBreedImage = response.message;
+        console.log('Imagem do cachorro:', this.selectedBreedImage);
+      },
+      (error) => {
+        console.log('Erro ao obter a imagem do cachorro:', error);
+      }
+    );
+  }
+
+  gerarDog() {
+    this.fetchBreedImage();
+  }
 }
