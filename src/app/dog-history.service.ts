9a55c60dedc7e48f7b6f98c3703bc099478f1c4f
Dioney Junior <dioneyfbjunior@gmail.com>
2023-09-04 16:44:26 -0300

    add dog-history service

--- src/app/dog-history.service.ts
+++ src/app/dog-history.service.ts
@@ -5,12 +5,34 @@ import { Injectable } from '@angular/core';
 })
 export class DogHistoryService {
   private dogHistory: string[] = [];
+  historyLoaded = false;
 
   addDogToHistory(imageUrl: string): void {
-    this.dogHistory.push(imageUrl);
+    if (!this.historyLoaded) {
+      this.loadDogHistory();
+    }
+
+    this.dogHistory = [imageUrl].concat(this.dogHistory);
+
+    this.saveDogHistory();
   }
 
   getDogHistory(): string[] {
+    if (!this.historyLoaded) {
+      this.loadDogHistory();
+    }
+
     return this.dogHistory;
+  }
+
+  private loadDogHistory() {
+    const history = localStorage.getItem('dogHistory');
+    if (history) {
+      this.dogHistory = JSON.parse(history);
+    }
+  }
+
+  private saveDogHistory() {
+    localStorage.setItem('dogHistory', JSON.stringify(this.dogHistory));
   }
 }
